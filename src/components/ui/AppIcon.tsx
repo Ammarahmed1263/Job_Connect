bb91e4881ecca87f78a4b256b660a3a61ea6271d
AmmarAhmed <ammarahmed1263@gmail.com>
2025-06-05 00:01:57 +0300

    update app icon and prevent failure with wrong input

--- src/components/ui/AppIcon.tsx
+++ src/components/ui/AppIcon.tsx
@@ -1,7 +1,5 @@
-// components/Icon.tsx
 import React, { FC } from "react";
 import { SvgProps } from "react-native-svg";
-
 import {
   home,
   home_outline,
@@ -16,10 +14,13 @@ import {
   case_outline,
   case as case_,
   bell,
-  bell_outline
+  bell_outline,
+  arrow_left,
+  arrow_right,
+  share
 } from "@assets/icons";
+import AppText from "./AppText";
 
-// Map names to components
 const icons: Record<string, FC<SvgProps>> = {
   home: home,
   "home-outline": home_outline,
@@ -34,7 +35,10 @@ const icons: Record<string, FC<SvgProps>> = {
   case: case_,
   "case-outline": case_outline,
   bell: bell,
-  "bell-outline": bell_outline
+  "bell-outline": bell_outline,
+  "arrow-left": arrow_left,
+  "arrow-right": arrow_right,
+  share: share
 };
 
 interface IconProps extends SvgProps {
@@ -45,6 +49,14 @@ interface IconProps extends SvgProps {
 
 const AppIcon: FC<IconProps> = ({ name, size = 24, color = "black", ...props }) => {
   const SvgIcon = icons[name];
+
+  if (!SvgIcon) {
+    if (__DEV__) {
+      console.warn(`[AppIcon] Invalid icon name passed: "${name}"`);
+    }
+    return <AppText style={{ fontSize: size, color }}>?</AppText>;
+  }
+
   return <SvgIcon width={size} height={size} color={color} {...props} />;
 };
 
